0b493b829087bccc508af31398e5e8c11fe2d951
Roger Issonaga <roger.issonaga@gmail.com>
2022-12-16 22:12:40 -0300

    turned to installable binary #9

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,1 +1,6 @@
-print("Executing entry point for dundie...")
+
+def main():
+    print("Executing dundi from entry point.")
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -7,4 +7,9 @@ setup(
     description="Reward Point System for Dunder Mifflin",
     author="Roger Issonaga",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    }
 )
