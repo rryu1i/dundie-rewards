8294e0f9e276fa3ed0942b23d562b76603b4cb14
roger <80974860+rryu1i@users.noreply.github.com>
2022-12-17 09:11:01 -0300

    turned to installable binary and added CLI entry points #9 (#12)
    * turned to installable binary #9
    
    * turned to installable binary #9
    
    * turned to installable binary #9
    
    * added CLI entry points #9
    
    closes #9

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,1 +1,40 @@
-print("Executing entry point for dundie...")
+import argparse
+
+
+def load(filepath):
+    """Loads data from filepath to the database"""
+    try:
+        with open(filepath) as f:
+            for line in f:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found {e}")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin Rewards CLI",
+        epilog="Enjoy and use with cautious.",
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="The subcommand to run",
+        choices=("load", "show", "send"),
+        default="help"
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load"
+    )
+    args = parser.parse_args()
+
+    try:
+        globals()[args.subcommand](args.filepath)
+    except KeyError:
+        print("Subcommand is invalid.")
+
+
+if __name__ == "__main__":
+    main()
