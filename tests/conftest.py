0f9a222de720c0703bce176f4a6fc42e888f0e89
Roger Issonaga <roger.issonaga@gmail.com>
2022-12-18 20:51:50 -0300

    Add finalizers, setup and teardown to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 MARKER = """\
 unit: Mark unit tests
 integration: Mark integration tests
@@ -10,3 +12,10 @@ low: Low priority
 def pytest_configure(config):
     for line in MARKER.split("\n"):
         config.addinivalue_line('markers',line)
+
+
+@pytest.fixture(autouse=True)
+def go_to_tmpdir(request):  # injecao de dependencias - o pytest que injeta
+    tmpdir = request.getfixturevalue("tmpdir")  # o pytest ja tem um diretorio temporario
+    with tmpdir.as_cwd():  # changing working directory
+        yield  # protocolo de generators
--- tests/test_load.py
+++ tests/test_load.py
@@ -1,11 +1,45 @@
 import pytest
 from dundie.core import load
 from .constants import PEOPLE_FILE
+import uuid
+import os
+
+
+@pytest.fixture(scope="function", autouse=True)  # se tiver autouse = True,
+#todos os testes dentro desse arquivo irao usar essa fixture,
+# caso queira aplicar apenas em determinada function, tirar o autouse e
+# fazer o test_load receber create_new_file
+def create_new_file(tmpdir):
+    file_ = tmpdir.join("new_file.txt")
+    file_.write("isso 'e sujeira...")
+    yield
+    file_.remove()
 
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load(request):
     """Test load function."""
+
+    filepath = f"arquivo_indesejado-{uuid.uuid4()}.txt"
+    request.addfinalizer(lambda: os.unlink(filepath))  # apos finalizar o test
+    # o os.unlink apaga o arquivo, mesma coisa que utilizar a fixture acima.
+
+    with open(filepath, "w") as f:
+        f.write("Dados uteis somente para o teste")
+
     assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == 'J'
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load2():
+    """Test load function."""
+
+    with open(f"arquivo_indesejado-{uuid.uuid4()}.txt", "w") as f:
+        f.write("Dados uteis somente para o teste")
+
+    assert len(load(PEOPLE_FILE)) == 2
+    assert load(PEOPLE_FILE)[0][0] == 'J'
+
