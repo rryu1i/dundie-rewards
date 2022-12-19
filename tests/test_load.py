bd6628257c97426ceb3fb2bab0c04b9643d8e4d1
Roger Issonaga <roger.issonaga@gmail.com>
2022-12-18 21:10:45 -0300

    Making an error

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,11 +1,24 @@
 import pytest
 from dundie.core import load
 from .constants import PEOPLE_FILE
+import uuid
+import os
+
 
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load_positive_has_2_people(request):
     """Test load function."""
     assert len(load(PEOPLE_FILE)) == 2
-    assert load(PEOPLE_FILE)[0][0] == 'J'
+
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_first_name_starts_with_j(request):
+    """Test load function."""
+    assert len(load(PEOPLE_FILE)) == 2
+
+
+
