06c08b74c93abc947331327cd640b79af082ac7b
roger <80974860+rryu1i@users.noreply.github.com>
2022-12-22 14:41:10 -0300

    Added click rich and testing (#18)

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,18 +1,18 @@
-from subprocess import CalledProcessError, check_output
-
 import pytest
+from click.testing import CliRunner
+
+from dundie.cli import load, main
+from tests.constants import PEOPLE_FILE
+
+cmd = CliRunner()
 
 
 @pytest.mark.integration
 @pytest.mark.medium
 def test_load_positive_call_load_command():
-    """Test comand load"""
-    out = (
-        check_output(["dundie", "load", "tests/assets/people.csv"])
-        .decode("utf-8")
-        .split("\n")
-    )
-    assert len(out) == 2
+    """Test command load"""
+    out = cmd.invoke(load, PEOPLE_FILE)
+    assert "Dunder Mifflin Associates" in out.output
 
 
 @pytest.mark.integration
@@ -20,8 +20,6 @@ def test_load_positive_call_load_command():
 @pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
 def test_load_negative_call_load_command_with_wrong_params(wrong_command):
     """Test comand load"""
-    with pytest.raises(CalledProcessError) as e:
-        check_output(
-            ["dundie", wrong_command, "tests/assets/people.csv"]
-        ).decode("utf-8").split("\n")
-    assert "status 2" in str(e.getrepr())
+    out = cmd.invoke(main, wrong_command, PEOPLE_FILE)
+    assert out.exit_code != 0
+    assert f"No such command '{wrong_command}'." in out.output
